0e714f6b65b2c5dc439967a6bb4da5deb8f36ea3
afilimon <morpheeen@gmail.com>
2021-01-24 15:26:53 +0200

    added 2 unit tests

---
+++ WorkPlanning/WorkPlanning.Services.Tests/ValidationHelperTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using WorkPlanning.DA.Models;
+using WorkPlanning.Services.Helpers;
+using Xunit;
+
+namespace WorkPlanning.Services.Tests
+{
+    public class ValidationHelperTests
+    {
+
+        public ValidationHelperTests()
+        {
+
+        }
+
+        [Fact]
+        public void ValidateShift_ShiftOverlaps_ThrowsException()
+        {
+            var start = new DateTime(2021, 1, 1, 12, 0, 0, DateTimeKind.Utc);
+            var end = new DateTime(2021, 1, 1, 19, 0, 0, DateTimeKind.Utc);
+
+            var existingShifts = new List<ShiftModel>
+            {
+                new ShiftModel{
+                Guid=Guid.NewGuid(),
+                Start=new DateTime(2021, 1, 1, 17, 0, 0, DateTimeKind.Utc),
+                End=new DateTime(2021, 1, 1, 17, 5, 0, DateTimeKind.Utc)
+                }
+            };
+            var ex = Assert.Throws<Exception>(() => ValidationHelper.ValidateShift(start, end, existingShifts));
+            Assert.Equal(Errors.ShiftOverlapsWithExisting, ex.Message);
+        }
+
+        [Fact]
+        public void ValidateShift_ShiftIsInARow_ThrowsException()
+        {
+            var start = new DateTime(2021, 1, 1, 17, 0, 0, DateTimeKind.Utc);
+            var end = new DateTime(2021, 1, 1, 19, 0, 0, DateTimeKind.Utc);
+
+            var existingShifts = new List<ShiftModel>
+            {
+                new ShiftModel{
+                Guid=Guid.NewGuid(),
+                Start=new DateTime(2021, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                End=new DateTime(2021, 1, 1, 11, 5, 0, DateTimeKind.Utc)
+                }
+            };
+            var ex = Assert.Throws<Exception>(() => ValidationHelper.ValidateShift(start, end, existingShifts));
+            Assert.Equal(Errors.ShiftIsInARow, ex.Message);
+        }
+    }
+}
--- WorkPlanning/WorkPlanning.Services/Helpers/ValidationHelper.cs
+++ WorkPlanning/WorkPlanning.Services/Helpers/ValidationHelper.cs
@@ -9,12 +9,12 @@ namespace WorkPlanning.Services.Helpers
     {
         public static void ValidateShift(DateTime start, DateTime end, ICollection<ShiftModel> existingShifts)
         {
-            if (existingShifts.Any(s => start >= s.Start && end > s.Start || start < s.End && end > s.End))
+            if (existingShifts.Any(s => start <= s.Start && end > s.Start || start < s.End && end > s.End))
             {
                 //3.A worker can not work two shifts at the same time
                 throw new Exception(Errors.ShiftOverlapsWithExisting);
             }
-            if (existingShifts.Any(s => s.End < start.AddHours(-24)))
+            if (existingShifts.Any(s => (start-s.End).TotalHours<24))
             {
                 //4.A worker never has two shifts in a row.
                 throw new Exception(Errors.ShiftIsInARow);
